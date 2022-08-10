047065697bea464ea2f6f6a83b4d07760ebfbc1a
S3LT1KK <108350870+S3LT1KK@users.noreply.github.com>
2022-08-10 21:26:01 +0400

    added 4th block and fixed fonts and minor changes

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ import './style.css';
 import './src/header.css';
 import './src/main.css';
 import './src/camping.css';
+import './src/booking.css';
+import './src/micromodaltest.css';
+
+MicroModal.init({
+   /*disableScroll: true*/
+});
