85f6e1a48c4219a2e22304806f979895f3b16bb6
S3LT1KK <108350870+S3LT1KK@users.noreply.github.com>
2022-08-20 17:17:08 +0400

    added full site

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ import './src/main.css';
 import './src/camping.css';
 import './src/booking.css';
 import './src/micromodaltest.css';
+import './src/accordeon.css';
+import './src/footer.css';
+import './src/map.css';
+import './src/slider.css';
 
 MicroModal.init({
    disableScroll: true
@@ -47,3 +51,50 @@ const slider = new Swiper('.slider__bl', {
       prevEl: '.swiper-button-prev',
    },
 });
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 3,
+   slidesPerColumn: 2,
+   spaceBetween: '5%',
+   autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+   },
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
+
+
+const accordeon = () => {
+   const charItem = document.querySelectorAll('.characteristics__item');
+   charItem.forEach((i) => {
+      const charButton = i.querySelector('.characteristics__title');
+      const charContent = i.querySelector('.characteristics__description');
+
+      charButton.addEventListener('click', () => {
+         if (charContent.classList.contains('open')) {
+            charContent.style.height = '';
+         } else {
+            charContent.style.height = charContent.scrollHeight + 'px';
+         }
+         charButton.classList.toggle('active');
+         charContent.classList.toggle('open');
+
+      })
+   })
+
+}
+accordeon();
