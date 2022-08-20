1e4efdb25396a862d2f6816d3fd5b31b0230b8f7
S3LT1KK <108350870+S3LT1KK@users.noreply.github.com>
2022-08-20 10:08:58 +0400

    added all latest changes(menu burger, fixed slider, ornaments and etc)

--- main.js
+++ main.js
@@ -8,5 +8,42 @@ import './src/booking.css';
 import './src/micromodaltest.css';
 
 MicroModal.init({
-   /*disableScroll: true*/
+   disableScroll: true
+});
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
+
+document.querySelectorAll("li").forEach(function (elem) {
+   elem.addEventListener('click', function (ev) {
+      document.body.classList.remove('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+})
+
+const slider = new Swiper('.slider__bl', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 1,
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
 });
